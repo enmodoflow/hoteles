f162385738f893cd75cf60e4e20ee696f517411a
enmodoflow <enmodoflow@gmail.com>
2020-03-30 11:34:28 +0200

    meter datos en el diccionario

--- src/modulos/scraping.py
+++ src/modulos/scraping.py
@@ -5,6 +5,16 @@ import lxml
 import urllib
 import re
 
+# Mongodb
+from pymongo import MongoClient
+
+# Mongo URL Atlas
+MONGO_URL_ATLAS = 'mongodb+srv://lolo:root@cluster0-nqj9e.mongodb.net/test?retryWrites=true&w=majority'
+
+client = MongoClient(MONGO_URL_ATLAS, ssl_cert_reqs=False)
+db = client['examen_final']
+collection = db['examen_final']
+
 # Objetivo
 url = "https://www.escapadarural.com/casas-rurales-aisladas"
 # Recogida de HTML
@@ -13,28 +23,40 @@ hoteles = requests.get(url)
 # VAMOS A EMPEZAR A PREPARAR LA 'SOUP'
 soup = BeautifulSoup(hoteles.text, 'lxml')
 
-
-# contenedor = soup.find('div', attrs={'class': 'contentCol'})
-
-# titulo = soup.find_all('a', attrs={'class': 'c-result--link'})
-# for i in titulo:
-#     print(i.contents)
-
-# descripcion = soup.find_all('div', attrs={'class': 'c-result--abstract'})
-# for i in descripcion:
-#     print(i.find('p').contents)
-
-# alquiler = soup.find_all('div', attrs={'class': 'c-result--item--text'})
-# for i in alquiler:
-#     print(i.contents)
-
-link = soup.find_all('article', attrs={'class': 'c-resultSnippet JS_resultSnippet'})
-for i in link:
-    print(i.find('rel'))
+diccionario = {}
 
 
-# precio = soup.find_all('div', attrs={'class': 'c-price--average'})
-# for i in precio:
-#     print(i.contents[0])
+# sacar todos los elementos del scrapping, y irlos metiendo en un diccionario
+c = 0
+titulo = soup.find_all('a', attrs={'class': 'c-result--link'})
+for i in titulo:
+    if c < 3:
+        diccionario['titulo'+ str(c)] = i.contents
+        c +=1
+
+co = 0
+descripcion = soup.find_all('div', attrs={'class': 'c-result--abstract'})
+for i in descripcion:
+    if co < 3:
+        diccionario['descripcion'+ str(co)] = i.find('p').contents
+        co += 1
+
+con = 0        
+alquiler = soup.find_all('div', attrs={'class': 'c-result--item--text'})
+for i in alquiler:
+    if con < 3:
+        diccionario['alquiler'+ str(con)] = i.contents
+        con += 1
+
+cont = 0
+precio = soup.find_all('div', attrs={'class': 'c-price--average'})
+for i in precio:
+    if cont < 3:
+        diccionario['precio'+ str(cont)] = i.contents[0]
+        cont += 1
+
+
+# insertar recogida de datos en un diccionario
+collection.insert_one(diccionario)
 
 
